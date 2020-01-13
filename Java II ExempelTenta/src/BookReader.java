9d72aeba2bb93c7694675c2364a43adf5d71d9c7
Thomas Karlsson <thomas@altho.se>
2020-01-13 19:39:26 +0100

    Added a searh menu that uses the search function with  input and output dialogs.

--- Java II ExempelTenta/src/BookReader.java
+++ Java II ExempelTenta/src/BookReader.java
@@ -1,11 +1,14 @@
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -34,12 +37,16 @@ public class BookReader extends JFrame {
 		//add(mb, BorderLayout.NORTH);
 		JMenu menu = new JMenu("Sort");
 		mb.add(menu);
+		JMenu smenu = new JMenu("Search");
+		mb.add(smenu);
 		JMenuItem s1 = new JMenuItem("By chapter number");
 		menu.add(s1);
 		JMenuItem s2 = new JMenuItem("By text lengt");
 		menu.add(s2);
 		JMenuItem s3 = new JMenuItem("By chapter heading");
 		menu.add(s3);
+		JMenuItem se1 = new JMenuItem("Search text...");
+		smenu.add(se1);
 
 		JPanel bookTitlePanel = new JPanel( new GridLayout(2,1));
 		bookTitlePanel.add(new JTextField("Title:" + "\n" + book.getTitle()));
@@ -55,6 +62,7 @@ public class BookReader extends JFrame {
 		s1.addActionListener(e -> {book.sortByChapterNumber(); printBook();});
 		s2.addActionListener(e -> {book.sortByTextLength(); printBook();});
 		s3.addActionListener(e -> {book.sortByChapterHeading(); printBook();});
+		se1.addActionListener(e -> searchInBook());
 
 	}
 	
@@ -62,6 +70,24 @@ public class BookReader extends JFrame {
 		textArea.setText("Chapters" + "\n");
 		for (Chapter c : book.getAllChapters())
 			textArea.append(c.getChapterNumber() + "\t" + c.getChapterHeading() + "\n");
+
+	}
+	
+	private void searchInBook() {
+		String searchString = JOptionPane.showInputDialog(this, "Enter search string:");
+		if ( searchString != null ) {
+			ArrayList<Chapter> foundChapters = book.search(searchString);
+			JDialog d = new JDialog(this, "Search result");
+			d.setSize(300, 300);
+			d.setLocationRelativeTo(null);
+			JTextArea ta = new JTextArea();
+			d.add(ta);
+			
+			for (Chapter c : foundChapters) {
+				ta.append(c.getChapterNumber() + "\t" + c.getChapterHeading() + "\n");
+			}
+			d.setVisible(true);
+		}
 
 	}
 
